b28a9045a74c7d9d8062eab8e298a51398e2a94f
Tashila <tashila.pathum@gmail.com>
2023-03-14 12:04:10 +0530

    Lists complete

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { Text, StyleSheet, View, FlatList} from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState('Tashila');
-  const [person, setPerson] = useState({name: 'Jack', age: 30});
-
-  const clickHandler = () => {
-    setName('Tashila');
-  }
+  const [people, setPeople] = useState([
+    {name: 'Ron', key: '0'},
+    {name: 'Tom', key: '1'},
+    {name: 'Ann', key: '2'},
+    {name: 'Ben', key: '3'}
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>My name is {name} and I'm {person.age} years old</Text>
-      <TextInput 
-        style={styles.inputStyle}
-        placeholder='Your name'
-        onChangeText={(val) => setName(val)}/>
-      <View style={styles.buttonStyle}>
-        <Button title='Change' onPress={clickHandler} />
-      </View>
-      <StatusBar style="auto" />
+      <FlatList
+      data={people}
+      renderItem={({ item }) => (
+        <Text style={styles.item}>{item.name}</Text>
+      )}
+      />
     </View>
   );
 }
@@ -32,15 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonStyle: {
-    borderRadius: 16,
-    marginTop: 16
-  },
-  inputStyle: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 8,
-    width: 200
+  item: {
+    padding: 16,
+    backgroundColor: 'blue',
+    margin: 8
   }
 });
